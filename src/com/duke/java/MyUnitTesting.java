e562eb47f44a6d2f7344786a8878c07e2dd180a3
Haifeng Zheng <Dad@JennyPC>
2024-04-20 14:03:49 -0400

    this just testing

--- src/com/duke/java/MyUnitTesting.java
+++ src/com/duke/java/MyUnitTesting.java
@@ -2,9 +2,10 @@ package com.duke.java;
 
 public class MyUnitTesting {
 
-	// test git checking
+	// test git checking, testing
+	
 	public static void main(String[] args) {
-		System.out.println("this is a testing!");
+		System.out.println("this is a testing! testing again!!!!");
 
 	}
 
